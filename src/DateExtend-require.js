7c50cbb668e7b3459fee575ff32fc64a0ad2cd82
HETUAN <hpwtuan@foxmail.com>
2016-10-10 20:34:26 +0800

    增加时间相减和返回新的时间对象的方法

--- src/DateExtend-require.js
+++ src/DateExtend-require.js
@@ -78,6 +78,17 @@ define([], function () {
         return result;
     }
 
+    //将两个时间相减返回毫秒数
+    Date.prototype.SubToMilliseconds = function (date) {
+        return this.getTime() - date.getTime();
+    }
+
+    //返回一个新的时间对象
+    Date.prototype.Copy = function () {
+        var d = new Date(this.Formate("yyyy-MM-dd hh:mm:ss.S"));
+        return d;
+    }
+    
     // 对Date的扩展，将 Date 转化为指定格式的String
     // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
     // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
